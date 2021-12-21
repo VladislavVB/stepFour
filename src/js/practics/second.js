ae9ec4e6d354f9c143546b137880d47cecf409bf
Vlad Vlad <burdukovskij.vlad@mail.ru>
2021-12-21 15:35:01 +0300

    point

--- src/js/practics/second.js
+++ src/js/practics/second.js
@@ -1,8 +1,9 @@
 let offset = 1;
-let limit = 10;
+let limit = 3;
 let fullData = [];
 let dataOnPage = [];
 let currentData = [];
+// let activePage = 1;
 let temp = "";
 const dataBtn = document.querySelectorAll(".data-btn");
 
@@ -18,7 +19,7 @@ dataBtn.forEach((elem) => {
 const getRes = async (url) => {
   const tableDataHead = document.querySelector(".table__data-head");
   tableDataHead.innerHTML = "";
- 
+
   temp = "";
   tableDataHead.innerHTML = `
   <div class="spinner-border text-light load-spiner" role="status">
@@ -27,7 +28,6 @@ const getRes = async (url) => {
   `;
   const responseSm = await fetch(url, {});
   const dataPerson = await responseSm.json();
-  console.log(dataPerson);
   fullData = dataPerson;
 
   tableDataHead.innerHTML = `
@@ -39,18 +39,20 @@ const getRes = async (url) => {
     <th scope="col headTabel" onclick="sortUser('phone')" >phone</th>
   </tr>
   `;
-  currentData = fullData
+  currentData = fullData;
   fillData();
 };
 
 const fillData = () => {
-  console.log(currentData);
-  let copyData = currentData.slice((offset - 1) * limit, (offset - 1) * limit + limit);
-  console.log(copyData);
+  let copyData = currentData.slice(
+    (offset - 1) * limit,
+    (offset - 1) * limit + limit
+  );
   const filterPagination = document.querySelector(".filter-pagination");
   filterPagination.innerHTML = "";
   filterPagination.innerHTML = getPaginator(
-    Math.ceil(currentData.length / limit), currentData
+    Math.ceil(currentData.length / limit),
+    currentData
   );
 
   const tableBody = document.querySelector(".table__body");
@@ -73,7 +75,6 @@ const fillData = () => {
   const searchPersonWrapper = document.querySelector(".search__person-wrapper");
   userInfoLine.forEach((elem, index) => {
     elem.onclick = () => {
-      console.log(index);
       detailPerson.innerHTML = `
         <h3>Выбран пользователь ${copyData[index].firstName}</h3>
         <p class="detail__person-descp">Описание: </br>${copyData[index].description}</p>
@@ -89,19 +90,23 @@ const fillData = () => {
     <div class="search__person">
       <input class="search-input" type="text" name="" id="">
       <button onclick="search()" >Найти</button>
+      <button onclick="searchRemove()" >Сбросить</button>
     </div>
   `;
-  disabelBtn()
+  disabelBtn();
 };
 
-const getPaginator = (pageCount) => {
+const getPaginator = (pageCount, ) => {
+  console.log(offset);
   let res = `
   <ul class="pagination">
   <li onclick="rollPage('prev')" class="page-item page-item-prev"><a class="page-link">Previous</a></li>
   `;
   for (let i = 1; i <= pageCount; i++) {
-
+    // activePage = i
+    // console.log(activePage);
     // if (2 >= pageCount) {
+    // console.log(pageCount);
     res += `<li onclick="getDataPage(${i})" class="page-item page-item-page"><a class="page-link">${i}</a></li>`;
     // }
   }
@@ -114,8 +119,8 @@ const getPaginator = (pageCount) => {
 };
 
 const getDataPage = (i) => {
-  console.log(i);
   offset = i;
+  console.log(i);
   fillData();
 };
 
@@ -129,12 +134,15 @@ const rollPage = (side) => {
       break;
   }
   fillData();
+  getPaginator(Math.ceil(currentData.length / limit), currentData);
 };
 
 const disabelBtn = () => {
   const pageItemNext = document.querySelector(".page-item-next");
   const pageItemPrev = document.querySelector(".page-item-prev");
-  console.log(offset);
+  // console.log(offset + 'wwwwwwwwwwwwww');
+  // console.log(Math.ceil(currentData.length / limit));
+
   if (offset == 1) {
     pageItemPrev.classList.add("disabel");
     pageItemNext.classList.remove("disabel");
@@ -145,6 +153,12 @@ const disabelBtn = () => {
     pageItemNext.classList.remove("disabel");
     pageItemPrev.classList.remove("disabel");
   }
+
+  if (Math.ceil(currentData.length / limit) == 1) {
+    // console.log('rty');
+    pageItemPrev.classList.add("disabel");
+    pageItemNext.classList.add("disabel");
+  }
 };
 
 const search = () => {
@@ -152,30 +166,38 @@ const search = () => {
   // let searchArr = fullData;
   const searchInput = document.querySelector(".search-input");
   let valueInput = searchInput.value;
-  console.log(valueInput);
-  // console.log(fullData);
   const searchArr = fullData.filter(
     (elem) =>
       elem.firstName.toUpperCase().indexOf(valueInput.toUpperCase()) != -1
   );
-  console.log(searchArr);
-  currentData = searchArr
+  currentData = searchArr;
   fillData();
 };
 
-let sortingMethod = 'asc';
+const searchRemove = () => {
+  offset = 1;
+  let valueInput = "";
+  const searchArr = fullData.filter(
+    (elem) =>
+      elem.firstName.toUpperCase().indexOf(valueInput.toUpperCase()) != -1
+  );
+  currentData = searchArr;
+  fillData();
+};
+
+let sortingMethod = "asc";
 
 const sortUser = (str) => {
-  console.log(sortingMethod);
-  console.log(str);
-  // fullData.id.sort();
-  console.log(5555);
-  if (sortingMethod === 'asc') {
-    currentData = fullData.sort((a, b) => (a[str] > b[str]) - (a[str] < b[str]))
-    sortingMethod = 'desc'
+  if (sortingMethod === "asc") {
+    currentData = fullData.sort(
+      (a, b) => (a[str] > b[str]) - (a[str] < b[str])
+    );
+    sortingMethod = "desc";
   } else {
-    currentData = fullData.sort((a, b) => (a[str] < b[str]) - (a[str] > b[str]))
-    sortingMethod = 'asc'
+    currentData = fullData.sort(
+      (a, b) => (a[str] < b[str]) - (a[str] > b[str])
+    );
+    sortingMethod = "asc";
   }
-  fillData()
+  fillData();
 };
